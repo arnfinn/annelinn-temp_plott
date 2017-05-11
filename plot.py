c160fb58cd97809f7898425d9550374ccd404f4d
Arnfinn <arnfinn.steindal@uit.no>
2017-05-11 22:16:14 +0200

    scriptet kan nå spise input

--- plot.py
+++ plot.py
@@ -2,10 +2,14 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
+
 #import seaborn
 #seaborn.set(style='ticks')
 
 #plt.style.use('ggplot')
+#print(plt.style.available)
+#plt.style.use('grayscale')
 
 SIZE = 14
 MEDIUM_SIZE = 16
@@ -19,7 +23,8 @@ plt.rc('ytick', labelsize=SIZE)          # fontsize of the tick labels
 plt.rc('legend', fontsize=SIZE)          # legend fontsize
 plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
-myfile = open("data.txt","r")
+
+myfile = open(sys.argv[1],"r")
 lines = myfile.readlines()
 myfile.close()
 
@@ -41,40 +46,56 @@ for i in lines:
 
 x = np.arange(len(date))
 
-linestyle = ["-","--","-.",":"]
+bw = True
+col = ["#a6611a", "#dfc27d", "#80cdc1", "#018571"]
+linestyle = ["-","-","-","-"]
+if bw:
+    col = ["black","black","black","black"]
+    linestyle = ["-","--","-.",":"]
+#linestyle = ["-","--","-.",":"]
+
 k = 0
-linje1 = plt.plot(x,aar2012, label = "2012", ls = linestyle[k], color = "black")
+linje1 = plt.plot(x,aar2012, label = "2012", ls = linestyle[k], color = col[k])
 k += 1
-linje2 = plt.plot(x,aar2013, label = "2013", ls = linestyle[k], color = "black")
+linje2 = plt.plot(x,aar2013, label = "2013", ls = linestyle[k], color = col[k])
 k += 1
-linje3 = plt.plot(x,aar2014, label = "2014", ls = linestyle[k], color = "black")
+linje3 = plt.plot(x,aar2014, label = "2014", ls = linestyle[k], color = col[k])
 
-early = [106,75,90]
-middle = [111,89,95]
-late = [119,97,102]
-
-early_val = [aar2012[early[0]],aar2013[early[1]],aar2014[early[2]]]
-middle_val = [aar2012[middle[0]],aar2013[middle[1]],aar2014[middle[2]]]
-late_val = [aar2012[late[0]],aar2013[late[1]],aar2014[late[2]]]
-
-punkt1 = plt.scatter(early,early_val, c="black", label = "Early")
-punkt2 = plt.scatter(middle, middle_val, c = "black", marker = "v", label = "Middle")
-punkt3 = plt.scatter(late, late_val, c = "black", marker = "D", label = "Late")
-
-first_legend = plt.legend(handles=[punkt1, punkt2, punkt3], loc=4, frameon=False)
-plt.gca().add_artist(first_legend)
 
 plot_lines = [linje1, linje2, linje3]
 #second_legend = 
 #plt.legend()
-plt.legend([l[0] for l in plot_lines], ["2012","2013","2014"], loc=2)
 #handles=[linje1, linje2, linje3], loc = 1)
 #plt.gca().add_artist(second_legend)
 
 #plt.ylabel("Akkumulerte døgngrader")
-plt.ylabel("Accumulated degree days ($^\circ$C)")
-#plt.xlabel("Dato")
-plt.ylim(0,1000)
+if sys.argv[1] == "data.txt":
+    early = [106,75,90]
+    middle = [111,89,95]
+    late = [119,97,102]
+
+    early_val = [aar2012[early[0]],aar2013[early[1]],aar2014[early[2]]]
+    middle_val = [aar2012[middle[0]],aar2013[middle[1]],aar2014[middle[2]]]
+    late_val = [aar2012[late[0]],aar2013[late[1]],aar2014[late[2]]]
+
+    punkt1 = plt.scatter(early,early_val, c="black", label = "Early")
+    punkt2 = plt.scatter(middle, middle_val, c = "black", marker = "v", label = "Middle")
+    punkt3 = plt.scatter(late, late_val, c = "black", marker = "D", label = "Late")
+
+    first_legend = plt.legend(handles=[punkt1, punkt2, punkt3], loc=4, frameon=False)
+    plt.gca().add_artist(first_legend)
+    plt.ylabel("Accumulated degree days ($^\circ$C)")
+    #plt.xlabel("Dato")
+    plt.ylim(0,1000)
+    location = 2
+    locMnd = -70
+else:
+    plt.ylabel("Average day temperature ($^\circ$C)")
+    plt.ylim(0,25)
+    location = 1
+    locMnd = -1.5
+
+plt.legend([l[0] for l in plot_lines], ["2012","2013","2014"], loc=location)
 plt.xlim(0,len(date))
 
 
@@ -85,8 +106,8 @@ plt.xticks(mndSkift,"")
 #plt.xticklabels(mnd)
 
 for i in range(len(mnd)):
-    plt.text(mndSkift[i]+10, -70, mnd[i])
+    plt.text(mndSkift[i]+10, locMnd, mnd[i])
 
 plt.tight_layout()
-plt.savefig("plot.png")
+plt.savefig(sys.argv[1].split(".")[0] + ".png")
 
