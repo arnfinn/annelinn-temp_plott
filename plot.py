8b6b580fd4dc78c922e0640fcc4b013a61fe7ab3
Arnfinn <arnfinn.steindal@uit.no>
2017-05-11 21:40:13 +0200

    perfect

--- plot.py
+++ plot.py
@@ -43,11 +43,11 @@ x = np.arange(len(date))
 
 linestyle = ["-","--","-.",":"]
 k = 0
-plt.plot(x,aar2012, label = "2012", ls = linestyle[k], color = "black")
+linje1 = plt.plot(x,aar2012, label = "2012", ls = linestyle[k], color = "black")
 k += 1
-plt.plot(x,aar2013, label = "2013", ls = linestyle[k], color = "black")
+linje2 = plt.plot(x,aar2013, label = "2013", ls = linestyle[k], color = "black")
 k += 1
-plt.plot(x,aar2014, label = "2014", ls = linestyle[k], color = "black")
+linje3 = plt.plot(x,aar2014, label = "2014", ls = linestyle[k], color = "black")
 
 early = [106,75,90]
 middle = [111,89,95]
@@ -57,20 +57,29 @@ early_val = [aar2012[early[0]],aar2013[early[1]],aar2014[early[2]]]
 middle_val = [aar2012[middle[0]],aar2013[middle[1]],aar2014[middle[2]]]
 late_val = [aar2012[late[0]],aar2013[late[1]],aar2014[late[2]]]
 
-plt.scatter(early,early_val, c="black", label = "Early")
-plt.scatter(middle, middle_val, c = "black", marker = "v", label = "Middle")
-plt.scatter(late, late_val, c = "black", marker = "D", label = "Late")
+punkt1 = plt.scatter(early,early_val, c="black", label = "Early")
+punkt2 = plt.scatter(middle, middle_val, c = "black", marker = "v", label = "Middle")
+punkt3 = plt.scatter(late, late_val, c = "black", marker = "D", label = "Late")
 
-plt.legend()
+first_legend = plt.legend(handles=[punkt1, punkt2, punkt3], loc=4, frameon=False)
+plt.gca().add_artist(first_legend)
 
-plt.ylabel("Akkumulerte døgngrader")
+plot_lines = [linje1, linje2, linje3]
+#second_legend = 
+#plt.legend()
+plt.legend([l[0] for l in plot_lines], ["2012","2013","2014"], loc=2)
+#handles=[linje1, linje2, linje3], loc = 1)
+#plt.gca().add_artist(second_legend)
+
+#plt.ylabel("Akkumulerte døgngrader")
+plt.ylabel("Accumulated degree days ($^\circ$C)")
 #plt.xlabel("Dato")
 plt.ylim(0,1000)
 plt.xlim(0,len(date))
 
 
 mndSkift = [0,31,61,92,123]
-mnd = ["Mai","Juni","Juli","Aug.", "Sep."]
+mnd = ["May","June","July","Aug.", "Sep."]
 #plt.xticks(mndSkift, mnd)
 plt.xticks(mndSkift,"")
 #plt.xticklabels(mnd)
